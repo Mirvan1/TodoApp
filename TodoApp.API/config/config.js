5aff547387adef4021a72415c54be400ef0a6fb9
Mirvan1 <mirvan.sadiqli@gmail.com>
2024-05-31 22:25:50 +0300

    1st commit

--- TodoApp.API/config/config.js
+++ TodoApp.API/config/config.js
@@ -23,9 +23,9 @@ console.log("Sequelize configuration:", JSON.stringify({
     }
 }, null, 2));
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'mysql',
-    protocol: 'mysql',
+const sequelize = new Sequelize('postgres://uc1kpf4aju7shh:p4988210d048d7703bd8db8e3b107a09c0fff074e8196bde6879eba770d03f72f@cat670aihdrkt1.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d29o6dhb50c9jp', {
+    dialect: 'postgres',
+    protocol: 'postgres',
     dialectOptions: {
         ssl: {
             require: true,
