919e47efd102950894f3f89d821a2cd5c7d45837
Mirvan1 <mirvan.sadiqli@gmail.com>
2024-05-31 12:57:44 +0300

    db config

--- TodoApp.API/config/database.js
+++ TodoApp.API/config/database.js
@@ -1,16 +1,26 @@
 const { Sequelize } = require('sequelize');
 
 // Set up the connection
-const sequelize = new Sequelize('ToDoDB', 'postgres', '12345', {
-    host: 'localhost',
+// const sequelize = new Sequelize('ToDoDB', 'postgres', '12345', {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     logging: false, // Turn off SQL logging in console,
+//     port:5432,
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    logging: false, // Turn off SQL logging in console,
-    port:5432,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false 
+        }
     }
 });
 
